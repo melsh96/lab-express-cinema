bf061c22c04d9f08e101837a7adbe0bdca79addb
Melodie Shahrvari <mshahrvari@gmail.com>
2022-01-07 00:27:12 +0100

    Iteration done

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const movie = require("../models/Movie.model");
+const mongoose = require("mongoose");
 
 router.get("/", (req, res) => {
   movie
@@ -12,6 +13,23 @@ router.get("/", (req, res) => {
       });
     })
     .catch((e) => console.error(e));
+});
+
+router.get("/:id", (req, res) => {
+  const isValidId = mongoose.isValidObjectId(req.params.id);
+  const id = req.params.id;
+  if (isValidId) {
+    movie
+      .findById(id)
+      .then((movie) => {
+        res.render("oneMovie", {
+          movie: movie,
+        });
+      })
+      .catch((e) => console.error(e));
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
